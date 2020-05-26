a734ed421eb11cc0f20386e5f23526e1235757db
Kohei.Yaginuma <f.kohei.yaginuma@gmail.com>
2020-05-26 17:19:07 +0900

    空行追加

--- infrastructure/middlewares/googlemap.go
+++ infrastructure/middlewares/googlemap.go
@@ -13,6 +13,7 @@ func GoogleMapClient() echo.MiddlewareFunc {
 	return func(next echo.HandlerFunc) echo.HandlerFunc {
 		return func(c echo.Context) error {
 			apiKey := os.Getenv("GMAP_API_KEY")
+
 			cl, err := maps.NewClient(maps.WithAPIKey(apiKey))
 			if err != nil {
 				logrus.Fatalf("Error creating GoogleMap client: %v", err)
