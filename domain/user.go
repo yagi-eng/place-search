45fc3b21bef55c6e03380afcf58958a670b02549
Kohei.Yaginuma <f.kohei.yaginuma@gmail.com>
2020-06-02 22:59:18 +0900

    [#3]User.DeleteAtを削除

--- domain/user.go
+++ domain/user.go
@@ -6,11 +6,10 @@ import (
 
 // User ユーザ
 type User struct {
-	ID         uint       `gorm:"primary_key"`
-	LineUserID string     `json:"-"`
-	CreatedAt  time.Time  `json:"-"`
-	UpdatedAt  time.Time  `json:"-"`
-	DeletedAt  *time.Time `sql:"index"json:"-"`
+	ID         uint      `gorm:"primary_key"`
+	LineUserID string    `json:"-"`
+	CreatedAt  time.Time `json:"-"`
+	UpdatedAt  time.Time `json:"-"`
 
 	Favorites []Favorite
 }
