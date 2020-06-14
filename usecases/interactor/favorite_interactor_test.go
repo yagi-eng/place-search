69f6f35eed1f876dfdcae2e41d6b48d21fd2734e
Kohei.Yaginuma <f.kohei.yaginuma@gmail.com>
2020-06-14 18:05:50 +0900

    [#10]favoriteInteractorのテストケース作成

--- usecases/interactor/favorite_interactor_test.go
+++ usecases/interactor/favorite_interactor_test.go
@@ -8,6 +8,7 @@ import (
 	mock_ipresenter "github.com/yagi-eng/virtual-travel/mock/presenter"
 	mock_repository "github.com/yagi-eng/virtual-travel/mock/repository"
 	"github.com/yagi-eng/virtual-travel/usecases/dto/favoritedto"
+	"github.com/yagi-eng/virtual-travel/usecases/dto/googlemapdto"
 )
 
 const userIDSuccess = uint(10)
@@ -17,6 +18,47 @@ const placeID = "googlemapPlaceID123"
 const replyToken = "hogeToken"
 const userExists = true
 const isAlreadyAdded = false
+const isAlreadyRemoved = false
+
+// TestGet1
+// 正常系のみ
+func TestGet1(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	placeIDs := []string{placeID}
+	gmo := googlemapdto.Output{}
+	gmos := []googlemapdto.Output{gmo}
+
+	mockUserRepo := mock_repository.NewMockIUserRepository(ctrl)
+
+	mockFavoriteRepo := mock_repository.NewMockIFavoriteRepository(ctrl)
+	mockFavoriteRepo.EXPECT().
+		FindAll(lineUserID).
+		Return(placeIDs)
+
+	mockGoogleMapGW := mock_gateway.NewMockIGoogleMapGateway(ctrl)
+	mockGoogleMapGW.EXPECT().
+		GetPlaceDetailsAndPhotoURLs(placeIDs, true).
+		Return(gmos)
+
+	expected := favoritedto.GetOutput{
+		ReplyToken:       replyToken,
+		GoogleMapOutputs: gmos,
+	}
+	mockLinePrst := mock_ipresenter.NewMockILinePresenter(ctrl)
+	mockLinePrst.EXPECT().
+		GetFavorites(expected).
+		Return()
+
+	interactor := NewFavoriteInteractor(mockUserRepo, mockFavoriteRepo, mockGoogleMapGW, mockLinePrst)
+
+	in := favoritedto.GetInput{
+		ReplyToken: replyToken,
+		LineUserID: lineUserID,
+	}
+	interactor.Get(in)
+}
 
 // TestAdd1
 // ユーザは存在しておらず、お気に入り登録されていない場合
@@ -126,4 +168,77 @@ func TestAdd3(t *testing.T) {
 		PlaceID:    placeID,
 	}
 	interactor.Add(in)
+}
+
+// TestRemove1
+// ユーザが存在しており、お気に入り削除されていない場合
+func TestRemove1(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mock_repository.NewMockIUserRepository(ctrl)
+	mockUserRepo.EXPECT().
+		FindOne(lineUserID).
+		Return(userIDSuccess)
+
+	mockFavoriteRepo := mock_repository.NewMockIFavoriteRepository(ctrl)
+	mockFavoriteRepo.EXPECT().
+		Delete(userIDSuccess, placeID).
+		Return(!isAlreadyRemoved)
+
+	mockGoogleMapGW := mock_gateway.NewMockIGoogleMapGateway(ctrl)
+
+	expected := favoritedto.RemoveOutput{
+		ReplyToken:       replyToken,
+		UserExists:       userExists,
+		IsAlreadyRemoved: isAlreadyRemoved,
+	}
+	mockLinePrst := mock_ipresenter.NewMockILinePresenter(ctrl)
+	mockLinePrst.EXPECT().
+		RemoveFavorite(expected).
+		Return()
+
+	interactor := NewFavoriteInteractor(mockUserRepo, mockFavoriteRepo, mockGoogleMapGW, mockLinePrst)
+
+	in := favoritedto.RemoveInput{
+		ReplyToken: replyToken,
+		LineUserID: lineUserID,
+		PlaceID:    placeID,
+	}
+	interactor.Remove(in)
+}
+
+// TestRemove2
+// ユーザ検索に失敗した場合
+func TestRemove2(t *testing.T) {
+	ctrl := gomock.NewController(t)
+	defer ctrl.Finish()
+
+	mockUserRepo := mock_repository.NewMockIUserRepository(ctrl)
+	mockUserRepo.EXPECT().
+		FindOne(lineUserID).
+		Return(userIDFail)
+
+	mockFavoriteRepo := mock_repository.NewMockIFavoriteRepository(ctrl)
+
+	mockGoogleMapGW := mock_gateway.NewMockIGoogleMapGateway(ctrl)
+
+	expected := favoritedto.RemoveOutput{
+		ReplyToken:       replyToken,
+		UserExists:       !userExists,
+		IsAlreadyRemoved: !isAlreadyRemoved,
+	}
+	mockLinePrst := mock_ipresenter.NewMockILinePresenter(ctrl)
+	mockLinePrst.EXPECT().
+		RemoveFavorite(expected).
+		Return()
+
+	interactor := NewFavoriteInteractor(mockUserRepo, mockFavoriteRepo, mockGoogleMapGW, mockLinePrst)
+
+	in := favoritedto.RemoveInput{
+		ReplyToken: replyToken,
+		LineUserID: lineUserID,
+		PlaceID:    placeID,
+	}
+	interactor.Remove(in)
 }
